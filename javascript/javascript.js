80192c2a451d22ec7cd80cb891327efda6461656
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-06-11 23:49:36 +0300

    devamını oku kısmı bitti

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
+	$(".message-box-2").hide();
   $(".read-more").on('click',function(){
-    $(".message-box").toggle(700);
+    $(".message-box-2").slideToggle(700);
+    $(this).text( $(this).text() === "Devamını Oku" ? 
+    "Daralt" : "Devamını Oku" );
+    $(".message-shadow").hide();
   })
 
 })
