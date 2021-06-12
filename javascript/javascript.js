fe2d9713721c065e73f2492a15b39753ae4f616c
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-06-12 15:26:23 +0300

    v3.0

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	$(".message-box-2").hide();
   $(".read-more").on('click',function(){
     $(".message-box-2").slideToggle(700);
-    $(this).text( $(this).text() === "Devamını Oku" ? 
-    "Daralt" : "Devamını Oku" );
-    $(".message-shadow").hide();
+   // $(this).text( $(this).text() === "Devamını Oku" ? "Daralt" : "Devamını Oku" );
+    $(".message-shadow").slideToggle(800);
+    $(this).toggleClass('off');
   })
 
 })
