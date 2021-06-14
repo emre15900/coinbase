d9069ac7e452bc79b0e0cadf7e65e98d91bff7bb
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-06-14 11:56:00 +0300

    search butonu ve grafik filter

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  // Message Box Toggle
 	$(".message-box-2").hide();
   $(".read-more").on('click',function(){
     $(".message-box-2").slideToggle(700);
@@ -7,5 +8,32 @@ $(document).ready(function(){
     $(".message-shadow").slideToggle(800);
     $(this).toggleClass('off');
   })
+
+  // Times Active
+  $(".time-value").on('click',function(){
+    $(this).addClass('active').siblings().removeClass('active')
+  })
+
+  // Times Filter
+  $('.graphic-item').hide();
+  $('.twenty-four-hour').show();
+  $('.time-value').click(function(){
+    const value = $(this).attr('data-filter');
+    if (value == 'all'){
+      $('.graphic-item').hide('100');
+    }
+    else{
+      $('.graphic-item').not('.'+value).hide('100');
+      $('.graphic-item').filter('.'+value).show('100');
+    }
+  })
+
+  // Search 
+  $(".search-button").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".coin-item").filter(function () {
+      $(this).toggle($(this).text().toLocaleLowerCase().indexOf(value) > -1);
+    });
+  });
 
 })
